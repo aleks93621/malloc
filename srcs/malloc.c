9ec66828e9327d3634709e0a35b519bfaddfe0e5
Aleksandar ALEKSOV <aaleksov@e1r1p8.42.fr>
2019-09-17 14:13:40 +0200

    Suppression de quelques lignes

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -6,7 +6,7 @@
 /*   By: aaleksov <aaleksov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 12:07:22 by aaleksov          #+#    #+#             */
-/*   Updated: 2019/09/17 12:48:40 by aaleksov         ###   ########.fr       */
+/*   Updated: 2019/09/17 14:13:17 by aaleksov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,12 @@ int main() {
     int i = 0;
     char *str;
 
-    // str = (char*)mmap(0, nb + 1, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-
-    // str = (char*)malloc(sizeof(char) * 42);
     while (i < 1024) {
         str = (char*)malloc(1024);
         str[0] = 42;
         i++;
     }
-    printf("%d\n", getpagesize());
-    // str[i] = '\0';
-    // ft_printf("%s\n", str);
+    // printf("%d\n", getpagesize());
 
     return (0);
 }
